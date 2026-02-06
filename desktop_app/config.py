d7a933bdb76954d28285d59d864c7178bd0891f7
luisHidalgo-git <hidalgovazquezluisangel2004@gmail.com>
2026-02-06 07:16:08 -0600

    no appdesk

--- desktop_app/config.py
+++
@@ -1,7 +0,0 @@
-API_BASE_URL = "http://localhost:8000/api"
-
-APP_NAME = "Sistema de Tickets"
-APP_VERSION = "1.0.0"
-
-WINDOW_WIDTH = 1200
-WINDOW_HEIGHT = 700
--- start_server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Iniciando servidor Django..."
-python3 manage.py runserver 0.0.0.0:8000
